2113a51f0afaca9fd1fa7f752f6a9429d8289cf9
mkindt <mkindt@unca.edu>
2013-02-25 23:59:37 -0500

    added ctwitter lib and set up search input

--- javascripts/app/app.js
+++ javascripts/app/app.js
@@ -18,7 +18,46 @@
     $.fn.foundationClearing         ? $doc.foundationClearing() : null;
 
     $.fn.placeholder                ? $('input, textarea').placeholder() : null;
-     $('#menu').jqDock();
+     
+    $('#menu').jqDock();
+    
+    var count = 0;
+    var rows = 0;
+
+    $(".locations").click(function() {
+    	var toAdd = $("input:checked").val();
+      $("footer").append("<p>"+toAdd+"</p>");
+      window.open("about.html", "_parent");
+    });
+    
+    $(".locator").click(function() {
+    	var placeName = $("input").val();
+      $("footer").append("<p>"+placeName+"</p>");
+    
+      var twitter = new ctwitter.CTwitter();
+      console.log(placeName);
+      twitter.stream("statuses/filter", { track:placeName }, function (stream){
+        stream.on("data", function(tweet) {
+          console.log(tweet.text);
+          if (count < 10){
+            $(".content").append("<p class='tweet' style='display:none'>"+tweet.text+"</p>");
+            $(".tweet:eq("+count+")").fadeIn(1200);
+            count = count + 1;
+          }
+          else {
+            if (rows===10){
+              rows = 0;
+            }
+            $(".tweet:eq("+rows+")").fadeOut(600, function() {
+              $(".tweet:eq("+rows+")").replaceWith(
+                "<p class='tweet' style='display:none'>"+tweet.text+"</p>");
+                $(".tweet:eq("+rows+")").fadeIn(600);
+                rows = rows + 1;
+            });
+          }
+        });
+      });
+    });   
   });
 
   // UNCOMMENT THE LINE YOU WANT BELOW IF YOU WANT IE8 SUPPORT AND ARE USING .block-grids
