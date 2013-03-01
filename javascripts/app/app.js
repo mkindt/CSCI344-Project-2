20981432a6e380a0bb35555bbb8c1cf15cffd13d
mkindt <mkindt@unca.edu>
2013-02-28 21:58:24 -0500

    completed cleanup of js file, ready for testing with JSLint

--- javascripts/app/app.js
+++ javascripts/app/app.js
@@ -4,21 +4,7 @@
   var $doc = $(document),
       Modernizr = window.Modernizr;
 
-  $(document).ready(function() {
-    $.fn.foundationAlerts           ? $doc.foundationAlerts() : null;
-    $.fn.foundationButtons          ? $doc.foundationButtons() : null;
-    $.fn.foundationAccordion        ? $doc.foundationAccordion() : null;
-    $.fn.foundationNavigation       ? $doc.foundationNavigation() : null;
-    $.fn.foundationTopBar           ? $doc.foundationTopBar() : null;
-    $.fn.foundationCustomForms      ? $doc.foundationCustomForms() : null;
-    $.fn.foundationMediaQueryViewer ? $doc.foundationMediaQueryViewer() : null;
-    $.fn.foundationTabs             ? $doc.foundationTabs({callback : $.foundation.customForms.appendCustomMarkup}) : null;
-    $.fn.foundationTooltips         ? $doc.foundationTooltips() : null;
-    $.fn.foundationMagellan         ? $doc.foundationMagellan() : null;
-    $.fn.foundationClearing         ? $doc.foundationClearing() : null;
-
-    $.fn.placeholder                ? $('input, textarea').placeholder() : null;
-     
+  $(document).ready(function () {
     //$('#menu').jqDock();
     var dockOptions2 =
       { align: 'right' // vertical menu, with expansion LEFT from a fixed RIGHT edge
@@ -28,7 +14,7 @@
     
     var count = 0;
     var rows = 0;
-    var blech;
+    var cityblock;
     $(".locations").click(function() {
     	var toAdd = $("input:checked").val();
       $("footer").append("<p>"+toAdd+"</p>");
@@ -38,36 +24,34 @@
     $(".locator").click(function() {
       $("#searchbox").fadeOut(600, function() {
         $("#returnSearch").append(
-          "<a href='about.html' title='HOME'><h3>Search Again?</h3></a>");
+          "<a href='about.html' title='HOME'>You searched: "+inputTrend+"<h3>  Search Again?</h3></a>");
       });
       var inputTrend;
     	inputTrend = $("input").val();
-      $("footer").append("<p>"+inputTrend+"</p>");   
+      //$("#returnSearch").append("<p>This search:"+inputTrend+"</p>");
       var twitter = new ctwitter.CTwitter();
       // coordinates of NYC, Los Angeles and Asheville
       var locationCoords = new Array("40.71455%2C-74.007118%2C3mi", "34.05349%2C-118.245323%2C10mi","35.598461%2C-82.553139%2C30mi");
       for (var i = 0; i<3; i++) {
-      twitter.stream("statuses/filter", { location:[locationCoords[i]], track:[inputTrend] }, function (stream){
-        stream.on("data", function(tweet) {
-          console.log(tweet.text);
-          if (count >= 0){
-            blech = ".losangeles";
-          }
-          if (count >= 10){
-            blech = ".asheville";
-          }
-          if (count >= 20){
-            blech = ".newyork";
-          }
-          console.log(count, blech);
-         
-            $(blech).append("<p class='tweet' style='display:block'>"+tweet.text+"</p>");
+        twitter.stream("statuses/filter", { location:[locationCoords[i]], track:[inputTrend] }, function (stream){
+          stream.on("data", function(tweet) {
+            console.log(tweet.text);
+            if (count === 0){
+              cityblock = ".losangeles";
+            }
+            if (count === 10){
+              cityblock = ".asheville";
+            }
+            if (count === 20){
+              cityblock = ".newyork";
+            }
+            console.log(count, cityblock);
+            $(cityblock).append("<p class='tweet' style='display:block'>"+tweet.text+"</p>");
             //$(".tweet:eq("+count+")").fadeIn(1200);
             count = count + 1;
-    
+          });
         });
-      });
-    }
+      }
     });   
   });
 
